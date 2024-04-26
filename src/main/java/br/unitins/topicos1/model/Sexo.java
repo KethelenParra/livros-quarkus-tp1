6533ff9efffd195347550588c279ff95a4de3230
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-04-26 16:01:35 -0300

    Adicionando Sexo

---
+++ src/main/java/br/unitins/topicos1/model/Sexo.java
@@ -0,0 +1,33 @@
+package br.unitins.topicos1.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT) // Quarkus vai entender Enum como objeto na hora de retornar
+public enum Sexo {
+    MASCULINO (1, "Masculino"),
+    FEMININO (2, "Feminino");
+
+    private int id;
+    private String nome;
+
+    Sexo(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public static Sexo valueOf(Integer id) throws IllegalArgumentException{
+        for (Sexo sexo : Sexo.values()){
+            if (sexo.id == id)
+                return sexo;
+        }
+        throw new IllegalArgumentException("Id Sexo Inválido!");
+    }
+}
