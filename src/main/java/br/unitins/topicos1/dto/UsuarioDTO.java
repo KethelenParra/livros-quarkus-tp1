95e3e35cda89891c4cabd8f5878b570b12923b4a
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-06-15 04:11:09 -0300

    Atualizando

--- src/main/java/br/unitins/topicos1/dto/UsuarioDTO.java
+++ src/main/java/br/unitins/topicos1/dto/UsuarioDTO.java
@@ -3,7 +3,6 @@ package br.unitins.topicos1.dto;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record UsuarioDTO(
     @NotBlank(message = "O campo nome não pode estar nulo")
@@ -14,14 +13,12 @@ public record UsuarioDTO(
 
     @NotBlank(message = "O campo senha não pode estar nulo")
     String senha,
+    
     LocalDate dataNascimento,
 
-    @NotBlank(message = "O campo email não pode estar nulo")
     String email,
     TelefoneDTO telefone,
     Integer idSexo,
 
-    @NotBlank(message = "O campo CPF não pode estar nulo")
-    @Size(max = 11, min = 11, message = "O cpf tem que ter no mínimo 11 dígitos e deve conter apenas os números")
     String cpf
 ) { }
