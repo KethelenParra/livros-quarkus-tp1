af8b9c917874f87a974a97587a2879f5dde301c7
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-06-15 02:40:10 -0300

    Fazendo atualizações em Auth - Add logs

--- src/main/java/br/unitins/topicos1/resource/auth/AuthResource.java
+++ src/main/java/br/unitins/topicos1/resource/auth/AuthResource.java
@@ -1,9 +1,12 @@
 package br.unitins.topicos1.resource.auth;
 
+import org.jboss.logging.Logger;
+
 import br.unitins.topicos1.dto.Response.UsuarioResponseDTO;
 import br.unitins.topicos1.dto.auth.AuthUsuarioDTO;
 import br.unitins.topicos1.service.hash.HashService;
 import br.unitins.topicos1.service.jwt.JwtService;
+import br.unitins.topicos1.validation.ValidationException;
 import br.unitins.topicos1.service.ClienteService;
 import br.unitins.topicos1.service.FuncionarioService;
 import jakarta.inject.Inject;
@@ -32,22 +35,35 @@ public class AuthResource {
     @Inject
     public JwtService jwtService;
 
+    private static final Logger LOG = Logger.getLogger(AuthResource.class);
+
     @POST
     public Response login(AuthUsuarioDTO dto) {
-        String hash = hashService.getHashSenha(dto.senha());
+        try {
+            String hashSenha = hashService.getHashSenha(dto.senha());
 
-        UsuarioResponseDTO usuario = null;
-        // perfil 1 = funcionario
-        if (dto.perfil() == 1) {
-            usuario = funcionarioService.login(dto.username(), hash);
-        } else if (dto.perfil() == 2) { // cliente
-            usuario = clienteService.login(dto.username(), hash);
-        } else {
-            return Response.status(Status.NOT_FOUND).build();
+            UsuarioResponseDTO usuario = null;
+    
+            if(dto.perfil() == 1){
+                usuario = funcionarioService.login(dto.username(), hashSenha);
+            } else if (dto.perfil() == 2){
+                usuario = clienteService.login(dto.username(), hashSenha);
+            } else {
+                return Response.status(Status.NOT_FOUND).header("Perfil", "perfis: 1-funcionario ou 2-cliente").build();
+            }
+    
+            if(usuario != null){
+                return Response.ok(usuario).header("Authorization", jwtService.generateJwt(dto, usuario))
+                                .status(Status.CREATED)
+                                .build();
+            } else {
+                return Response.status(Status.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            LOG.error("Erro durante o login. Verifique seu username ou senha!");
+            throw new ValidationException("Verificando", "Erro durante o login. Verifique seu username ou senha!");
         }
-        return Response.ok(usuario)
-            .header("Authorization", jwtService.generateJwt(usuario, dto.perfil()))
-            .build();
     }
-
 }
+
+
