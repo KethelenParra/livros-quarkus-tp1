299a2d5884de7af3d1ab0988c8628a9c992dfb22
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-06-22 01:20:33 -0300

    update testes

--- src/test/java/br/unitins/topicos1/resource/ClienteResourceTest.java
+++ src/test/java/br/unitins/topicos1/resource/ClienteResourceTest.java
@@ -1,13 +1,14 @@
 package br.unitins.topicos1.resource;
 import org.junit.jupiter.api.Test;
 
-import br.unitins.topicos1.dto.AlterarSenhaDTO;
-import br.unitins.topicos1.dto.AlterarUsernameDTO;
 import br.unitins.topicos1.dto.ClienteDTO;
 import br.unitins.topicos1.dto.TelefoneDTO;
+import br.unitins.topicos1.dto.Response.ClienteResponseDTO;
+import br.unitins.topicos1.service.ClienteService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -19,6 +20,9 @@ import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 public class ClienteResourceTest {
+
+    @Inject
+    public ClienteService clienteService;
  
     @Test
     @TestSecurity(user = "teste", roles = "Funcionario")
@@ -68,7 +72,7 @@ public class ClienteResourceTest {
     }
 
     @Test
-    @TestSecurity(user = "testeCliente", roles = "Cliente")
+    @TestSecurity(user = "teste", roles = "Cliente")
     public void createTest() {
         TelefoneDTO telefoneDTO = new TelefoneDTO("85", "99887766");
         ClienteDTO dto = new ClienteDTO(
@@ -126,58 +130,33 @@ public class ClienteResourceTest {
     }
     
     @Test
-    @TestSecurity(user = "testeCliente", roles = "Cliente")
+    @TestSecurity(user = "teste", roles = "Cliente")
     public void deleteTest() {
+        TelefoneDTO telefoneDTO = new TelefoneDTO("62", "33226623");
+        ClienteDTO dto = new ClienteDTO(
+            "978653",
+            "Rua 110",
+            "Toncatins",
+            "Gurupi",
+            "Coral montenegro",
+            "carolassa",
+            LocalDate.parse("2003-03-10"),
+            "carolas@gmail.com",
+            "132", 
+            "21232212",
+            2, 
+            telefoneDTO
+        );
+
+        ClienteResponseDTO response = clienteService.create(dto);
         
         given()
             .when()
-            .pathParam("id", 12)
+            .pathParam("id", response.id())
             .delete("/clientes/{id}")
             .then()
             .statusCode(204);
     }
 
-    @Test
-    public void alterarSenhaTest() {
-        AlterarSenhaDTO dto = new AlterarSenhaDTO("123", "321");
-        /*Login: João */
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjoxLCJzdWIiOiJKb2FvMTAiLCJncm91cHMiOlsiQ2xpZW50ZSJdLCJleHAiOjE3NDk5NDc2ODksImlhdCI6MTcxODQxMTY4OSwianRpIjoiZWUxMzg1YTQtNzkxZC00ZTFlLTg5NDgtMTUxODczZTY4MTlmIn0.IlmaZXIXicDpmubrSy3kk2qkN3E0sUB0Q1vjtbhHEw90gqAE4L6adwRV1yvea0SVCjHTOzkL3dcLudud_KHzfwpwvLoCLO-XNbcuwbH1AhSNUmuo_2yQ6wjKnW4_RoZ5Hlo4Q89DK_m1xebThPc2jl2nYqFq4LeEDdo19x1ddHO3L-lgmWtEwlVkPlSr9lYhUYTbdd7ugpvmILpy_Tbe41Kp5Jfi-CGPIMGAdiWgJeufiHl9ea__RTzHx4pNO21CDOKfDYQFRryh16vNWCzD_7DsoGcLX67CW9-r9IBRlWsCn3bQQSOb5Ud25cskVRVxDxQJ1xjlEje8GxdJxIRLvw";
-        given()
-        .header("Authorization", "Bearer " + token)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(dto)
-            .when()
-            .patch("/clientes/search/alterar-senha")
-            .then()
-            .statusCode(204);
-    }
-
-    @Test
-    public void alterarUsernameTest() {
-        AlterarUsernameDTO dto = new AlterarUsernameDTO("321", "novoUsername");
-
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjoxLCJzdWIiOiJKb2FvMTAiLCJncm91cHMiOlsiQ2xpZW50ZSJdLCJleHAiOjE3NDk5NDc2ODksImlhdCI6MTcxODQxMTY4OSwianRpIjoiZWUxMzg1YTQtNzkxZC00ZTFlLTg5NDgtMTUxODczZTY4MTlmIn0.IlmaZXIXicDpmubrSy3kk2qkN3E0sUB0Q1vjtbhHEw90gqAE4L6adwRV1yvea0SVCjHTOzkL3dcLudud_KHzfwpwvLoCLO-XNbcuwbH1AhSNUmuo_2yQ6wjKnW4_RoZ5Hlo4Q89DK_m1xebThPc2jl2nYqFq4LeEDdo19x1ddHO3L-lgmWtEwlVkPlSr9lYhUYTbdd7ugpvmILpy_Tbe41Kp5Jfi-CGPIMGAdiWgJeufiHl9ea__RTzHx4pNO21CDOKfDYQFRryh16vNWCzD_7DsoGcLX67CW9-r9IBRlWsCn3bQQSOb5Ud25cskVRVxDxQJ1xjlEje8GxdJxIRLvw";
-        given()
-            .header("Authorization", "Bearer " + token)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(dto)
-            .when()
-            .patch("/clientes/search/alterar-username")
-            .then()
-            .statusCode(204);
-    }
-
-    @Test
-    public void meuPerfilTest() {
-        /*Perfil: Juliana */
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjo1LCJzdWIiOiJqdWxpYW5hc2FudG9zIiwiZ3JvdXBzIjpbIkNsaWVudGUiXSwiZXhwIjoxNzQ5OTQ5NzI0LCJpYXQiOjE3MTg0MTM3MjQsImp0aSI6IjNkMjZjMmIyLWUzNGEtNGQ3My05Y2U1LTkwOTZjN2JiM2ZkZiJ9.a0vFnlyWQi47IKgRof0LVvGANBA1dekmYW8qLS9CVALabxNCSupo3pb3fE_Ne41c5j_pSe6T4YkRpa-7WizYw2j9runz5b2N0P0jyqB8eCjLO9iaQZxde97HIPFEW030uDRp7WJLStMbUGnihMHi9FnfC1U7NyfFXJM4ZOs4v52tv7MJkpWgZXm_kU6nuGLBeO8Y08oSxTYjKEavD2lf_7CemZx5f-s9FVKVx-aKl_9_ORsRi4Vw81b4ZbnG_CMuZXNU2_Ydthc5wDuRhepp85P4_z9RhvfVDMkmQ5cX731SkQAaKcuPZitTmklna5XrDWKw12DIuZN70lLBjUM2kw";
-        given()
-            .header("Authorization", "Bearer " + token)
-            .when()
-            .get("/clientes/search/meu-perfil")
-            .then()
-            .statusCode(200);
-    }
-    
 }
 
--- src/test/java/br/unitins/topicos1/resource/PedidoResourceTest.java
+++ src/test/java/br/unitins/topicos1/resource/PedidoResourceTest.java
@@ -1,21 +1,26 @@
 package br.unitins.topicos1.resource;
 
 import br.unitins.topicos1.dto.PedidoDTO;
-import br.unitins.topicos1.dto.CartaoCreditoDTO;
+import br.unitins.topicos1.dto.Response.PedidoResponseDTO;
+import br.unitins.topicos1.service.PedidoService;
 import br.unitins.topicos1.dto.ItemPedidoDTO;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @QuarkusTest
 public class PedidoResourceTest {
+
+    @Inject
+    public PedidoService pedidoService;
 
     @Test
     @TestSecurity(user = "teste", roles = "Funcionario")
@@ -49,86 +54,38 @@ public class PedidoResourceTest {
     }
 
     @Test
+    @TestSecurity(user = "teste", roles = "Cliente")
     public void createTest() {
         ItemPedidoDTO item = new ItemPedidoDTO(1L, 2);
         PedidoDTO dto = new PedidoDTO(4L, List.of(item));
 
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
+        PedidoResponseDTO response = pedidoService.create(dto);
+
         given()
-        .header("Authorization", "Bearer" + token)
+        .pathParam("id", response.id())
             .contentType(MediaType.APPLICATION_JSON)
             .body(dto)
             .when()
             .post("/pedidos")
             .then()
             .statusCode(201)
-            .body("cliente.id", is(4));
+            .body("cliente.id", is(4L));
     }
     
 
     @Test
+    @TestSecurity(user = "teste", roles = "Cliente")
     public void cancelarPedidoTest() {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
+        PedidoResponseDTO response = pedidoService.create(new PedidoDTO(
+            1L,
+            Arrays.asList(new ItemPedidoDTO(2L, 4))
+        ));
+        
         given()
-        .header("Authorization", "Bearer " + token)
             .when()
-            .delete("/pedidos/search/cancelar-Pedido")
+            .pathParam("id", response.id())
+            .delete("/pedidos/{id}/cancelar-Pedido")
             .then()
             .statusCode(204);
     }
-
-    @Test
-    public void meusPedidosTest() {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
-        given()
-        .header("Authorization", "Bearer " + token)
-            .when()
-            .get("/pedidos/search/meus-Pedidos")
-            .then()
-            .statusCode(200);
-    }
-
-    @Test
-    public void pagarBoletoTest() {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
-        given()
-            .header("Authorization", "Bearer " + token)
-            .when()
-            .patch("/pedidos/search/pagar-Boleto")
-            .then()
-            .statusCode(202);
-    }
-
-    @Test
-    public void pagarPixTest() {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
-        given()
-        .header("Authorization", "Bearer " + token)
-            .when()
-            .patch("/pedidos/search/pagar-Pix")
-            .then()
-            .statusCode(202);
-    }
-
-    @Test
-    public void pagarCartaoCreditoTest() {
-        CartaoCreditoDTO cartao = new CartaoCreditoDTO("0980909232", "teste", 325, LocalDate.parse("2023-10-10"), "123123", 1);
-
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1bml0aW5zLWp3dCIsImlkIjozLCJzdWIiOiJhbmFvbGl2ZWlyYSIsImdyb3VwcyI6WyJDbGllbnRlIl0sImV4cCI6MTc0OTk1ODM2MywiaWF0IjoxNzE4NDIyMzYzLCJqdGkiOiI0ZTk3YzVjYy1jNzRjLTQxMDUtYWM2YS0wN2QwMWExZTRhOTQifQ.naLkGeq8--RNi80UiJbXO5ssvALmt7FWmbzQ0IPTAxTA9KvZSmkicvDrVIG_bF2UT1273mPc29CelBLItOZDE9zCbJdTQl_YDf0p9ZDUqqgIoO3G7zE00BzO6JvccmStcHpcZc12ybyq1D7kcXzUnybZrqSJvE5r56od8xewE3UdhfQJ5El_0MEClxV8HqL5t0Y8wkt8CiXdxiTpANuuSfY8gkS-XCGQ4wzeSw4ZpXgFXT3WUr9WmgpVseAFSVGgzX8eazA21BwMRbM5sKV1UiRp2bBE-uuSeQvweL7QvT5grl4m6t2tuj1oUoHm-U1L7ZzQEQa9eXYjjnX3SRn-Mw";
-      
-        given()
-        .header("Authorization", "Bearer " + token)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(cartao)
-            .when()
-            .patch("/pedidos/search/pagar-Cartao-Credito")
-            .then()
-            .statusCode(202);
-    }
-
 }
